347b13c040e34b2891936d21d9d478febcfe82cc
Mrinal Roy <102941745+RoyMarmeto@users.noreply.github.com>
2023-08-05 11:43:43 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -362,7 +362,7 @@ upload_to_google_sheet() {
     # log "Data to send: $json_object"
 
     # data=$(cat /github/workspace/reports/manifest.json)
-    curl -X POST -H "Content-Type: application/json" -d "$json_object" "https://script.google.com/macros/s/AKfycbwJniKE3kalCPI4p4kZ_NGMe04tPNbYd7PWxIMBcvm6bdNVf4C6cAbLgOK_vKCipJ0l/exec"
+    curl -X POST -H "Content-Type: application/json" -d @<(echo "$json_object") "https://script.google.com/macros/s/AKfycbwJniKE3kalCPI4p4kZ_NGMe04tPNbYd7PWxIMBcvm6bdNVf4C6cAbLgOK_vKCipJ0l/exec"
 }
 
 ls /github/workspace/reports/
