ab1676e4aa8e5727e574f3c2e358426da1687698
Mrinal Roy <102941745+RoyMarmeto@users.noreply.github.com>
2023-08-27 23:17:40 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -379,7 +379,7 @@ upload_to_google_sheet() {
 	    log "This is a Pull Request to the default branch: $DEFAULT_BRANCH. Uploading to Google Sheets..."
      	    log "Sheet url: https://docs.google.com/spreadsheets/d/1kcA7iPXsEuzktgTpmcstz1ylUm5znWKx0X5eaPP_R3c"
 	  
-     	    curl -X POST -H "Content-Type: application/json" -d "$json_object" "https://script.google.com/macros/s/AKfycbwiZMxIddSJKpJAjkIl0SxgbcmjoQVyxhOGGV80FaSQx5W9ZUX0UavC96bUeeFytjjf/exec"
+     	    curl -X POST -H "Content-Type: application/json" -d "$json_object" "https://script.google.com/macros/s/AKfycbzLV0be7XLDUmK98gr6VWzmveIvnl4hLEJox3TXL2Q2hGoHzcqvqOdxo0e76u_N1Ak-/exec"
 	  else
 	    log "This is a Pull Request, but not to the default branch.  Skipping upload to Google Sheets."
 	  fi
